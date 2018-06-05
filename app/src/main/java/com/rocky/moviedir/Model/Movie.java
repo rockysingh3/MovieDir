8fb65f41e91d1d310d2ff66c7b9e59a9128e9771
rockysingh3 <rockygsingh3@gmail.com>
2018-06-05 15:07:51 -0400

    Added all the fields for the movie class. This is what you get back when calling the api.

--- app/src/main/java/com/rocky/moviedir/Model/Movie.java
+++ app/src/main/java/com/rocky/moviedir/Model/Movie.java
@@ -1,7 +1,27 @@
 package com.rocky.moviedir.Model;
 
-public class Movie {
+import java.io.Serializable;
 
-    public String h = "hello world";
-    public String j = "hfh;sa";
+public class Movie implements Serializable {
+
+    private static final long id = 1L;
+
+    private String title;
+    private String director;
+    private String year;
+    private String runTime;
+    private String imdbId;
+    private String poster;
+    private String genre;
+    private String writer;
+    private String actors;
+    private String plot;
+    private String rating;
+    private String dvdRelease;
+    private String productionCompany;
+    private String country;
+    private String awards;
+    private String tvRated;
+    private String movieType;
+
 }
